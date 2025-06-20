fe28bef1528bcd253933a45e1f5423f214c0aa7e
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-06-20 11:44:43 -0700

    add description field to NewTaskForm, update state to use an object, refactor handleChange & submitTaskData to use new state constant

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -1,24 +1,31 @@
 import { useState, useId } from 'react';
 import PropTypes from 'prop-types';
 
+const kFormData = {
+  title: '',
+  description: '',
+};
+
 const NewTaskForm = ({ onAddTask }) => {
-  const [title, setTitle] = useState('');
+  const [formData, setFormData] = useState(kFormData);
   const inputId = useId();
 
   const submitTaskData = (event) => {
     event.preventDefault();
-
-    const newTask = {
-      title,
-      description: ''
-    };
-
-    onAddTask(newTask);
-    setTitle('');
+    onAddTask(formData);
+    setFormData(kFormData);
   };
 
   const handleChange = (event) => {
-    setTitle(event.target.value);
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [inputName]: inputValue,
+      };
+    });
   };
 
   return (
@@ -26,13 +33,22 @@ const NewTaskForm = ({ onAddTask }) => {
       <section>
         <h2>Add a New Task</h2>
         <div>
-          <label htmlFor={`${inputId}-title`}></label>
+          <label htmlFor={`${inputId}-title`}>Title: </label>
           <input
             onChange={handleChange}
             type="text"
             name="title"
             id={`${inputId}-title`}
-            value={title} />
+            value={formData.title} />
+        </div>
+        <div>
+          <label htmlFor={`${inputId}-description`}>Description: </label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="description"
+            id={`${inputId}-description`}
+            value={formData.description} />
         </div>
         <div>
           <button type="submit">Add Task</button>
