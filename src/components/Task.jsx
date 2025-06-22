89027761cced4f0524daaee06397e6540583c465
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-06-22 13:56:24 -0700

    add specific state for new TaskDetails popup component, add TaskDetails component to Task, add new completedAt prop

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import TaskDetails from './TaskDetails';
 import './Task.css';
+import './TaskDetails.css';
 
-const Task = ({ id, title, isComplete, onTaskToggle, onTaskDelete, onTaskCompletionToggle}) => {
+const Task = ({ id, title, description, completedAt, isComplete, onTaskDelete, onTaskCompletionToggle}) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+  const completionStatus = isComplete ? `${completedAt}` : 'In Progress';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => onTaskToggle(id)}
+        onClick={() => setIsPopupOpen(true)}
       >
         {title}
       </button>
+      <TaskDetails isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)}>
+        <h2>Task Details</h2>
+        <ul className='task__details--list'>
+          <li><span>Title: </span>{title}</li>
+          <li><span>Description: </span>{description}</li>
+          <li><span>Completed at: </span>{completionStatus}</li>
+        </ul>
+      </TaskDetails>
       <button
         className="tasks__item__status button"
         onClick={() => onTaskCompletionToggle(id)}
@@ -31,8 +44,9 @@ const Task = ({ id, title, isComplete, onTaskToggle, onTaskDelete, onTaskComplet
 Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  completedAt: PropTypes.date,
   isComplete: PropTypes.bool.isRequired,
-  onTaskToggle: PropTypes.func.isRequired,
   onTaskDelete: PropTypes.func.isRequired,
   onTaskCompletionToggle: PropTypes.func.isRequired,
 };
